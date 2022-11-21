9f51ea8657fab0b6a1df3bd358d9dd6470bd586b
fasilmsageer <fasilmsageer@gmail.com>
2022-11-21 11:59:15 +0530

    exit added

--- src/Main.java
+++ src/Main.java
@@ -3,39 +3,63 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        char operator;
+        int operator;
         Double number1, number2, result;
 
         Scanner input = new Scanner(System.in);
 
         System.out.println("Choose an option : 1, 2, 3, 4 or 5");
         System.out.println(" 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division or 5 to exit");
-        operator = input.next().charAt(0);
+        operator = input.nextInt();
 
-        System.out.println("Enter first number: ");
-        number1 =input.nextDouble();
 
-        System.out.println("Enter second number: ");
-        number2 =input.nextDouble();
 
         switch (operator){
 
             case '1':
+                System.out.println("Enter first number: ");
+                number1 =input.nextDouble();
+
+                System.out.println("Enter second number: ");
+                number2 =input.nextDouble();
+
                 result = number1 + number2;
                 System.out.println(number1 + " + " + number2 + " = " + result);
                 break;
             case '2':
+                System.out.println("Enter first number: ");
+                number1 =input.nextDouble();
+
+                System.out.println("Enter second number: ");
+                number2 =input.nextDouble();
+
                 result = number1 - number2;
                 System.out.println(number1 + " - " + number2 + " = " + result);
                 break;
             case '3':
+                System.out.println("Enter first number: ");
+                number1 =input.nextDouble();
+
+                System.out.println("Enter second number: ");
+                number2 =input.nextDouble();
+
                 result = number1 * number2;
                 System.out.println(number1 + " * " + number2 + " = " + result);
                 break;
             case '4':
+                System.out.println("Enter first number: ");
+                number1 =input.nextDouble();
+
+                System.out.println("Enter second number: ");
+                number2 =input.nextDouble();
+
                 result = number1 / number2;
                 System.out.println(number1 + " / " + number2 + " = " + result);
                 break;
+            case '5':
+                System.out.println("Thank you");
+                System.exit(0);
+
         }
 
 
